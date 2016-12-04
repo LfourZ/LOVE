cbdb29a9e2dd4395f53a9392b9ccf0cb931f3041
lfourunderscore <lfourunderscore@gmail.com>
2016-12-04 07:28:49 +0100

    Bump

--- game-V0.1/main.lua
+++ game-V0.1/main.lua
@@ -2,27 +2,53 @@ function love.load()
 	--[[
 	CHANGES:
 	-Changed meter/pixel into a more realistic number (15/1)
+	-Now loads all physics objects at once, as well as new ones
+	-Added support for rendering and simulating polygons, circles and edges
+	-Window title is now game version
+	-Added examples for physics border
+	-Console now shows draws/frame and total used graphics memory in MB
 	]]--
 	world = love.physics.newWorld(0, 0, true)
 	love.physics.setMeter(15)
 
 	player = {
-		body = love.physics.newBody(world, 0, 0, "dynamic"),
+		body = love.physics.newBody(world, 500, 500, "dynamic"),
 		shape = love.physics.newCircleShape(15),
 	}
 
 	objects = {
 		wall = {
-			body = love.physics.newBody(world, 100, 100, "dynamic"),
-			shape = love.physics.newRectangleShape(100, 100, 50, 200),
+			body = love.physics.newBody(world, 500, 500, "dynamic"),
+			shape = love.physics.newRectangleShape(200, 200),
+			color = { 200, 200, 200 },
+			drawmode = "fill",
+			renderdist = 100,
+			mass = 0.0001
+		},
+		leftwall = {
+			body = love.physics.newBody(world, 1, 1081, "static"),
+			shape = love.physics.newRectangleShape(2, 1838),
+			color = { 0, 0, 0, 0 },
+			drawmode = "line",
+			renderdist = 1000,
+			mass = 10
+		},
+		topwall = {
+			body = love.physics.newBody(world, 0, 0, "static"),
+			shape = love.physics.newEdgeShape(0, 0, 5000, 0),
+			color = { 0, 0, 255 },
+			renderdist = 0,
+			mass = 10
 		}
 	}
+	for k, object in pairs(objects) do
+		object.fixture = love.physics.newFixture(object.body, object.shape, object.mass)
+	end
 
-	player.fixture = love.physics.newFixture(player.body, player.shape, 1)
-	objects.wall.fixture = love.physics.newFixture(objects.wall.body, objects.wall.shape, 1)
+	player.fixture = love.physics.newFixture(player.body, player.shape, 0.25)
 
 	posx, posy = 0, 0
-	love.window.setMode(1000, 1000, {resizable = true, msaa = 4, vsync = false})
+	love.window.setMode(1000, 1000, { resizable = true, msaa = 0, vsync = false })
 	wX, wY = love.window.getMode()
 	wXh, wYh = wX / 2, wY / 2
 	mapTileResolution = 500
@@ -69,6 +95,7 @@ function love.load()
 end
 
 function love.draw()
+	posx, posy = player.body:getPosition()
 	love.graphics.translate(wXh, wYh)
 	for k, v in pairs(map) do
 		for k2, v2 in pairs(v) do
@@ -78,15 +105,27 @@ function love.draw()
 			end
 		end
 	end
-	posx, posy = player.body:getPosition()
 	love.graphics.setColor(0, 255, 0)
 	love.graphics.print(love.timer.getFPS(), -wXh, -wYh, 0, 0.25)
 	love.graphics.setColor(193, 47, 14)
 	love.graphics.circle("fill", player.body:getX() - posx, player.body:getY() - posy, player.shape:getRadius())
-	love.graphics.setColor(50, 50, 50)
-	local polytbl = {objects.wall.body:getWorldPoints(objects.wall.shape:getPoints())}
-	love.graphics.polygon("fill", subtractPos(polytbl))
+	for name, object in pairs(objects) do
+		if object.shape:getType() == "polygon" then
+			local polytbl = { object.body:getWorldPoints(object.shape:getPoints()) }
+			love.graphics.setColor(object.color)
+			love.graphics.polygon(object.drawmode, subtractPos(polytbl))
+		elseif object.shape:getType() == "circle" then
+			love.graphics.setColor(object.color)
+			love.graphics.circle(object.drawmode, object.body:getX() - posx, object.body:getY() - posy, object.shape:getRadius())
+		elseif object.shape:getType() == "edge" then
+			love.graphics.setColor(object.color)
+			local polytbl = { object.body:getWorldPoints(object.shape:getPoints()) }
+			love.graphics.line(subtractPos(polytbl))
+		end
+	end
 	love.graphics.setColor(255, 255, 255)
+	local stats = love.graphics.getStats()
+	print(stats.drawcalls, stats.texturememory / 1024 / 1024)
 end
 
 function love.resize(Nx, Ny)
@@ -95,10 +134,11 @@ function love.resize(Nx, Ny)
 	mapTileRenderDistX = wXh
 	mapTileRenderDistY = wYh
 end
+
 function love.update(Dt)
 	if Dtt > physicsRate then
 		world:update(Dt)
-		Dtt = 0
+		Dtt = Dtt - physicsRate
 
 		if love.keyboard.isDown("w") then
 			player.body:applyForce(0, -movespeed)
@@ -109,6 +149,9 @@ function love.update(Dt)
 			player.body:applyForce(-movespeed, 0 )
 		elseif love.keyboard.isDown("d") then
 			player.body:applyForce(movespeed, 0)
+		end
+		if love.keyboard.isDown("space") then
+			player.body:setLinearVelocity(0, 0)
 		end
 	else
 		Dtt = Dtt + Dt
